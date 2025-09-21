5c4d71c386fd0ba449b47232349f3cb0d83a102b
Asad Rana <asadlukman246@gmail.com>
2025-09-21 15:25:41 +0530

    edits

--- Javascript/FrontEndTask/Popup/index.js
+++ Javascript/FrontEndTask/Popup/index.js
@@ -1,11 +1,39 @@
-var showDialog = false;
 
 const dialogContainer = document.getElementById('dialog-container');
+const dialog = document.getElementById('dialog');
 const showDialogButton = document.getElementById('popup-button');
 const closeButtons = document.querySelectorAll('.close-button')
+const firstCloseButton = closeButtons[0];
+const secondCloseButton = closeButtons[1];
+const backDropClick = document.getElementById('showBackDrop');
+const showCloseIcon = document.getElementById('showCloseIcon');
 
-closeButtons.forEach((btn) => btn.addEventListener('click',() => dialogContainer.style.display = 'none'));
+document.addEventListener('click', (e) => {
+    const target = e.target;
+    switch (target) {
+        case showDialogButton:
+            dialogContainer.style.display = 'flex';
+            break;
+        case firstCloseButton:
+        case secondCloseButton:
+            dialogContainer.style.display = 'none';
+            break;
+        case backDropClick:
+            dialogContainer.style.backgroundColor = target.checked ? 'rgba(0, 0, 0, 0.5)' : 'transparent';
+            break;
+        case showCloseIcon:
+            firstCloseButton.style.visibility = target.checked ? 'visible' : 'hidden';
+            break;
+        default:
+            if(document.getElementById('outSideClick').checked) dialogContainer.style.display = 'none';
+    }
+})
 
-showDialogButton.addEventListener('click', () => dialogContainer.style.display = 'flex')
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && document.getElementById('escapeClick').checked) {
+        dialogContainer.style.display = 'none';
+    }
+
+})
 
 
