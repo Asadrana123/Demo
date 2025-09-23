c43f26bff216030a5842cbdf97b6270be55a0535
Asad Rana <asadlukman246@gmail.com>
2025-09-23 20:19:30 +0530

    edits

--- Javascript/FrontEndTasks/TypeHead/index.js
+++ Javascript/FrontEndTasks/TypeHead/index.js
@@ -1,21 +1,38 @@
-const suggestionLength = 5;
-
 const input = document.getElementsByTagName('input')[0]
+const loader = document.getElementById('loader');
+const resultContainer = document.getElementById('results-container');
 var timeOutId = null;
 
 const fetchSearchResult = async (text) => {
-    if(text.length===0) return;
     try {
         const response = await fetch(`https://api.datamuse.com/sug?s=${text}`);
-        console.log(response);
         const searchResult = await response.json();
-        console.log(searchResult);
+        showResults(searchResult);
     } catch (err) {
         console.log(err);
     }
 }
 
+const showResults = (resultList) => {
+    const newNodes = resultList.map((result) => {
+        const node = document.createElement('div');
+        node.textContent = result.word;
+        node.setAttribute('class', 'result');
+        return node;
+    })
+    loader.style.display = 'none'
+    resultContainer.replaceChildren(...newNodes);
+}
+
+const showLoading = () =>{
+    loader.style.display = 'block';
+    resultContainer.style.display='none'
+}
+
+
 input.addEventListener('change', async (e) => {
+    if (e.target.value.length === 0) return;
+    showLoading();
     clearTimeout(timeOutId)
     timeOutId = setTimeout(() => {
         fetchSearchResult(e.target.value)
