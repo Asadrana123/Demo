e66d4a661a689bd7c08dd0ea3207755608abb20e
Asad Rana <asadlukman246@gmail.com>
2025-10-23 21:26:20 +0530

    enhance problem

--- Javascript/AsynchronousJavascript/Promises/settled/Problems/2.js
+++ Javascript/AsynchronousJavascript/Promises/settled/Problems/2.js
@@ -5,33 +5,35 @@
 // If it succeeds on retry → count as success.
 // If it fails again → stay failed.
 function job(id) {
-  return new Promise((resolve, reject) => {
-    const time = Math.floor(Math.random() * 900) + 100;
-    setTimeout(() => {
-      Math.random() > 0.5
-        ? resolve(`Job ${id} success`)
-        : reject({value:`Job ${id} failed`,id});
-    }, time);
-  });
+    return new Promise((resolve, reject) => {
+        const time = Math.floor(Math.random() * 900) + 100;
+        setTimeout(() => {
+            Math.random() > 0.5
+                ? resolve(`Job ${id} success`)
+                : reject({ value: `Job ${id} failed`, id });
+        }, time);
+    });
 }
 
 const start = Date.now();
 
 Promise.allSettled([
-  job(1),
-  job(2),
-  job(3),
-  job(4),
-  job(5)
-]).then((results)=>{
-   const result=results.reduce((acc,result)=>{
-         if(result.status==='fulfilled') acc.success++;
-         else{
-            
-            acc.failure++
-         }
-         return acc;
-   },{success:0,failure:0,time:Date.now()-start})
-   console.log(result);
+    job(1),
+    job(2),
+    job(3),
+    job(4),
+    job(5)
+]).then((results) => {
+    const result = results.reduce(async (acc, result) => {
+        if (result.status === 'fulfilled') acc.success++;
+        else {
+            acc.retry++;
+            const retriedResult = await job(result.value.id);
+            if (retriedResult.status === 'fulfilled') acc.success++;
+            else acc.failure++
+        }
+        return acc;
+    }, { success: 0, failure: 0, retry: 0, time: Date.now() - start })
+    console.log(result);
 })
 
