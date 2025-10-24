5d8fcd337d57a8d1b1cd7d0183e5984277110cad
Asad Rana <asadlukman246@gmail.com>
2025-10-24 18:10:14 +0530

    edits

--- Javascript/AsynchronousJavascript/Promises/settled/Problems/2.js
+++ Javascript/AsynchronousJavascript/Promises/settled/Problems/2.js
@@ -1,36 +1,32 @@
 function job(id) {
-    return new Promise((resolve, reject) => {
-        const time = Math.floor(Math.random() * 900) + 100;
-        setTimeout(() => {
-            Math.random() > 0.5
-                ? resolve(`Job ${id} success`)
-                : reject({ value: `Job ${id} failed`, id });
-        }, time);
-    });
+  return new Promise((resolve, reject) => {
+    const time = Math.floor(Math.random() * 900) + 100;
+    setTimeout(() => {
+      Math.random() > 0.5
+        ? resolve(`Job ${id} success`)
+        : reject(`Job ${id} failed`);
+    }, time);
+  });
 }
 
 const start = Date.now();
 
 Promise.allSettled([
-    job(1),
-    job(2),
-    job(3),
-    job(4),
-    job(5)
-]).then((results) => {
-   results.reduce(async (acc, result) => {
-        if (result.status === 'fulfilled') acc.success++;
-        else {
-            acc.retry++;
-            try{
-            const retriedResult = await job(result.value.id);
-            if (retriedResult.status === 'fulfilled') acc.success++;
-            }
-            catch(err){
-             acc.failure++   
-            }
-        }
-        return acc;
-    }, { success: 0, failure: 0, retry: 0, time: Date.now() - start }).then((result)=>console.log(result))
+  job(1),
+  job(2),
+  job(3),
+  job(4),
+  job(5)
+]).then((results)=>{
+   const result=results.reduce((acc,result)=>{
+         if(result.status==='fulfilled') acc.success++;
+         else acc.failure++
+         return acc;
+   },{success:0,failure:0,time:Date.now()-start})
+   console.log(result);
 })
 
+
+
+//https://chatgpt.com/c/68e68d3d-15b8-8323-9fb4-f4defbbc5120
+//https://www.perplexity.ai/search/const-obj-val-1-inc-function-v-1CcAiokDS.a6la6KUvM2ig
