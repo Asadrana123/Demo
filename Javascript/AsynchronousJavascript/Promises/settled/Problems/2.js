c4b7db509fcba1e565404e288359fbe9afbbb3ce
Asad Rana <asadlukman246@gmail.com>
2025-10-24 20:56:11 +0530

    enhance problem

--- Javascript/AsynchronousJavascript/Promises/settled/Problems/2.js
+++ Javascript/AsynchronousJavascript/Promises/settled/Problems/2.js
@@ -1,32 +1,46 @@
 function job(id) {
-  return new Promise((resolve, reject) => {
-    const time = Math.floor(Math.random() * 900) + 100;
-    setTimeout(() => {
-      Math.random() > 0.5
-        ? resolve(`Job ${id} success`)
-        : reject(`Job ${id} failed`);
-    }, time);
-  });
+    return new Promise((resolve, reject) => {
+        const time = Math.floor(Math.random() * 900) + 100;
+        setTimeout(() => {
+            Math.random() > 0.5
+                ? resolve(`Job ${id} success`)
+                : reject({ value: `Job ${id} failed`, id });
+        }, time);
+    });
 }
 
 const start = Date.now();
-
+let resultObj;
 Promise.allSettled([
-  job(1),
-  job(2),
-  job(3),
-  job(4),
-  job(5)
-]).then((results)=>{
-   const result=results.reduce((acc,result)=>{
-         if(result.status==='fulfilled') acc.success++;
-         else acc.failure++
-         return acc;
-   },{success:0,failure:0,time:Date.now()-start})
-   console.log(result);
+    job(1),
+    job(2),
+    job(3),
+    job(4),
+    job(5)
+]).then((results) => {
+        let failedId=[];
+        results.forEach((result)=>{
+               if(result.status==='rejected'){
+                    failedId.push(result.reason.id);
+               }
+        })
+    let noOfRetries=failedId.length;
+     Promise.allSettled(failedId.map((id)=>job(id)))
+     .then((results)=>{
+            failedId=[];
+            results.forEach((result)=>{
+               if(result.status==='rejected'){
+                    failedId.push(result.reason.id);
+               }
+        })
+        resultObj={
+                success:5-failedId.length,
+                failures:failedId.length,
+                retry:noOfRetries,
+                time:Date.now()-start
+            }
+        console.log(resultObj)
+    });
 })
 
 
-
-//https://chatgpt.com/c/68e68d3d-15b8-8323-9fb4-f4defbbc5120
-//https://www.perplexity.ai/search/const-obj-val-1-inc-function-v-1CcAiokDS.a6la6KUvM2ig
